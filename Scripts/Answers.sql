76712663d3bc96d8775c6c0ad2fe02b31fcb5b95
arinaldosegundo <arinaldosegundo@78c0d296-d796-11de-9fcc-cd5105d4569c>
2009-11-25 01:54:33 +0000

    Organizando answers

--- Scripts/Answers.sql
+++ Scripts/Answers.sql
@@ -1,22 +1,19 @@
---12 Consulta:
+--02
+--##
 
-SELECT F.FILME.TITULO AS FILME, VALUE(P).NOME AS DIRETOR
-FROM FILMES_INFO_TB F, TABLE(F.PROFISSIONAIS) P
-WHERE F.FILME.NUM_OSCARS > 2 AND VALUE(P) IS OF (ONLY DIRETOR_TY)
-ORDER BY F.FILME.TITULO;
+--03
+--##
 
---RESULTADO:
---FILME	DIRETOR
---Jogos mortais	Arinaldo da Silva
---Jogos mortais	George Marcelo
---O mascara	Claudio Batista
---O mascara 2	Claudio Batista
---Titanic	James Cameron
---Titanic	John Landau
---2012	John Smith
+--04
+--##
 
---7)
+--05
+--##
 
+--06
+--##
+
+--07
 ALTER TYPE FILME_TY
     DROP MEMBER FUNCTION duracaoEmHoras RETURN NUMBER CASCADE;
 
@@ -31,29 +28,50 @@ CREATE OR REPLACE TYPE BODY FILME_TY IS
         return self.duracao/60;
     END;
 END;
+--##
 
---20)
+--08
+--##
 
---Consulta:
+--09
+--##
 
-select f.titulo from FILMES_TB f
-  where f.duracaoEmHoras() > 2  
+--10
+--##
 
+--11
+--##
 
---Resultado:
+--12
+SELECT F.FILME.TITULO AS FILME, VALUE(P).NOME AS DIRETOR
+FROM FILMES_INFO_TB F, TABLE(F.PROFISSIONAIS) P
+WHERE F.FILME.NUM_OSCARS > 2 AND VALUE(P) IS OF (ONLY DIRETOR_TY)
+ORDER BY F.FILME.TITULO;
 
---TITULO
---Titanic
---2012
---Jogos mortais
---O mascara
---O mascara 2
+--RESULTADO:
+--FILME	DIRETOR
+--Jogos mortais	Arinaldo da Silva
+--Jogos mortais	George Marcelo
+--O mascara	Claudio Batista
+--O mascara 2	Claudio Batista
+--Titanic	James Cameron
+--Titanic	John Landau
+--2012	John Smith
+--##
 
+--13
+select f.filme.titulo , count(*) as num_alugueis 
+from emprestimos_stb f
+group by f.filme.titulo
+order by num_alugueis DESC  
 
---14)
+--FILME.TITULO	NUM_ALUGUEIS
+--O mascara 2	2
+--O mascara	1
+--Jogos mortais	1
+--##
 
---Consulta:
-
+--14
 SELECT VALUE(P).NOME AS ATOR
 FROM FILMES_INFO_TB F, TABLE(F.PROFISSIONAIS) P
 WHERE F.FILME.CATEGORIA='Terror' AND VALUE(P) IS OF (ONLY ATOR_TY)
@@ -64,70 +82,9 @@ ORDER BY ATOR;
 --ATOR
 --Heverton Stuart
 --Jorge Ter
+--##
 
-
---19)
-
---Consulta:
-
-SELECT F.FILME.TITULO, F.FILME.NUM_OSCARS AS OSCARS_FILME, TREAT(VALUE(P) AS ATOR_TY).NUM_OSCARS AS OSCARS_ATOR
-FROM FILMES_INFO_TB F, TABLE(F.PROFISSIONAIS) P
-WHERE VALUE(P).NOME='John Wayne' AND VALUE(P) IS OF (ONLY ATOR_TY)
---Resultado:
-
---FILME.TITULO	OSCARS_FILME	OSCARS_ATOR
---Troia	     1	1
---O mascara	 11	2
---O mascara 2	11	5
-
---5)
-
-CREATE OR REPLACE PROCEDURE CRIACLIENTE
-(
-    CPF_IN     IN CLIENTES_TB.CPF%TYPE,
-    NOME_IN    IN CLIENTES_TB.NOME%TYPE
-)
-AS
-BEGIN
-    INSERT INTO CLIENTES_TB VALUES
-    (
-        CLIENTE_TY
-            (
-                (SELECT COUNT(*) FROM CLIENTES_TB) + 1, --GERA UM CODIGO PARA O CLIENTE
-                NOME_IN,
-                CPF_IN,
-                NULL,
-                NULL,
-                NULL,
-                NULL,
-                NULL,
-                NULL
-         )
-);
-END;
-
---para rodar
-
-BEGIN
-CRIACLIENTE(11111111111, 'cliente st');
-END;
-
---Para verificar se adicionou
-
-select * from clientes_tb
-
---17
-
-select c.cliente.nome, t.* , e.*
-from clientes_info_tb c, table(c.cliente.emails) e, table(c.cliente.telefones) t,  TABLE(c.emprestimos) w
-where w.filme.titulo='Troia'
-
---Resultado
---CLIENTE.NOME	COLUMN_VALUE	COLUMN_VALUE
---Luiz Gonzaga	(83)3333-3333	lg@gmail.com
---Luiz Gonzaga	(83)8888-8888	lg@gmail.com
-
---15) pendente affff
+--15				PENDENTE!!!!!!!!!!!!!!!!!!!
 select t.categoria from
 (select y.filme.categoria as categoria, count(*) as ocorrencias
 from CLIENTES_INFO_TB c, TABLE(c.emprestimos) y
@@ -149,15 +106,46 @@ group by y.filme.categoria
 
 --CATEGORIA
 --Comedia
+--##
 
---13
-select f.filme.titulo , count(*) as num_alugueis 
-from emprestimos_stb f
-group by f.filme.titulo
-order by num_alugueis DESC  
+--16
+--##
 
---FILME.TITULO	NUM_ALUGUEIS
---O mascara 2	2
---O mascara	1
---Jogos mortais	1
+--17
+select c.cliente.nome, t.* , e.*
+from clientes_info_tb c, table(c.cliente.emails) e, table(c.cliente.telefones) t,  TABLE(c.emprestimos) w
+where w.filme.titulo='Troia'
 
+--Resultado
+--CLIENTE.NOME	COLUMN_VALUE	COLUMN_VALUE
+--Luiz Gonzaga	(83)3333-3333	lg@gmail.com
+--Luiz Gonzaga	(83)8888-8888	lg@gmail.com
+--##
+
+--18
+--##
+
+--19
+SELECT F.FILME.TITULO, F.FILME.NUM_OSCARS AS OSCARS_FILME, TREAT(VALUE(P) AS ATOR_TY).NUM_OSCARS AS OSCARS_ATOR
+FROM FILMES_INFO_TB F, TABLE(F.PROFISSIONAIS) P
+WHERE VALUE(P).NOME='John Wayne' AND VALUE(P) IS OF (ONLY ATOR_TY)
+--Resultado:
+
+--FILME.TITULO	OSCARS_FILME	OSCARS_ATOR
+--Troia	     1	1
+--O mascara	 11	2
+--O mascara 2	11	5
+--##
+
+--20
+select f.titulo from FILMES_TB f
+  where f.duracaoEmHoras() > 2  
+
+--Resultado:
+--TITULO
+--Titanic
+--2012
+--Jogos mortais
+--O mascara
+--O mascara 2
+--##
