de963f969d43c927393c99daa6ed5f6398b27f72
arinaldosegundo <arinaldosegundo@78c0d296-d796-11de-9fcc-cd5105d4569c>
2009-11-25 02:20:01 +0000

    Padronizando as consultas

--- Scripts/Answers.sql
+++ Scripts/Answers.sql
@@ -13,19 +13,17 @@
 --06
 --##
 
---07
+--07	OK
 ALTER TYPE FILME_TY
-    DROP MEMBER FUNCTION duracaoEmHoras RETURN NUMBER CASCADE;
-
+    DROP MEMBER FUNCTION DURACAOEMHORAS RETURN NUMBER CASCADE;
 
 ALTER TYPE FILME_TY
-    ADD MEMBER FUNCTION duracaoEmHoras RETURN NUMBER CASCADE;
-
+    ADD MEMBER FUNCTION DURACAOEMHORAS RETURN NUMBER CASCADE;
 
 CREATE OR REPLACE TYPE BODY FILME_TY IS
-    MEMBER FUNCTION  duracaoEmHoras  RETURN NUMBER IS
+    MEMBER FUNCTION  DURACAOEMHORAS  RETURN NUMBER IS
     BEGIN
-        return self.duracao/60;
+        RETURN SELF.DURACAO/60;
     END;
 END;
 --##
@@ -42,7 +40,7 @@ END;
 --11
 --##
 
---12
+--12	OK
 SELECT F.FILME.TITULO AS FILME, VALUE(P).NOME AS DIRETOR
 FROM FILMES_INFO_TB F, TABLE(F.PROFISSIONAIS) P
 WHERE F.FILME.NUM_OSCARS > 2 AND VALUE(P) IS OF (ONLY DIRETOR_TY)
@@ -59,11 +57,12 @@ ORDER BY F.FILME.TITULO;
 --2012	John Smith
 --##
 
---13
-select f.filme.titulo , count(*) as num_alugueis 
-from emprestimos_stb f
-group by f.filme.titulo
-order by num_alugueis DESC  
+--13	OK
+SELECT E.FILME.TITULO AS FILME, COUNT(*) AS NUM_ALUGUEIS
+FROM EMPRESTIMOS_STB E
+GROUP BY E.FILME.TITULO
+ORDER BY NUM_ALUGUEIS DESC
+
 
 --FILME.TITULO	NUM_ALUGUEIS
 --O mascara 2	2
@@ -71,11 +70,11 @@ order by num_alugueis DESC
 --Jogos mortais	1
 --##
 
---14
-SELECT VALUE(P).NOME AS ATOR
+--14	OK
+SELECT VALUE(P).NOME AS NOME_ATOR
 FROM FILMES_INFO_TB F, TABLE(F.PROFISSIONAIS) P
-WHERE F.FILME.CATEGORIA='Terror' AND VALUE(P) IS OF (ONLY ATOR_TY)
-ORDER BY ATOR;
+WHERE F.FILME.CATEGORIA = 'Terror' AND VALUE(P) IS OF (ONLY ATOR_TY)
+ORDER BY NOME_ATOR;
 
 --Resultado:
 
@@ -109,6 +108,23 @@ group by y.filme.categoria
 --##
 
 --16
+select f.categoria, count(*) as numvezes
+from filmes_tb f
+where f.codigo not in
+(select y.filme.codigo as codigo_filme
+from CLIENTES_INFO_TB c, TABLE(c.reservas) y
+where y.data_reserva > (SELECT SYSDATE - 730  FROM DUAL))
+group by f.categoria
+
+--730 eh o numero de dias em 2 anos
+
+
+--Resultado
+
+--CATEGORIA	NUMVEZES
+--Fim dos tempos	1
+--Drama	1
+--Comedia	2
 --##
 
 --17
@@ -137,9 +153,10 @@ WHERE VALUE(P).NOME='John Wayne' AND VALUE(P) IS OF (ONLY ATOR_TY)
 --O mascara 2	11	5
 --##
 
---20
-select f.titulo from FILMES_TB f
-  where f.duracaoEmHoras() > 2  
+--20	OK
+SELECT F.TITULO AS TITULO_FILME
+FROM FILMES_TB F
+WHERE F.DURACAOEMHORAS() > 2;  
 
 --Resultado:
 --TITULO
